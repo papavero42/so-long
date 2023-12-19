2f8fa7fd3725d040cf146866cd45c3525b8e393d
papavero42 <141956427+papavero42@users.noreply.github.com>
2023-12-19 19:54:30 +0100

    Add files via upload

---
+++ so_long.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdi-pint <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 18:29:55 by pdi-pint          #+#    #+#             */
+/*   Updated: 2023/12/19 18:29:58 by pdi-pint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "mlx/mlx.h"
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+
+# define WINDOW_WIDTH 800
+# define WINDOW_HEIGHT 600
+# define IMG_WIDTH 32
+# define IMG_HEIGHT 32
+
+typedef struct s_map
+{
+    int gift;
+    char **map;
+}               t_map;
+
+typedef struct s_coord
+{
+	int		x;
+	int		y;
+}			t_coord;
+
+typedef struct s_img
+{
+    void *exit;
+    void *wall;
+    void *coll;
+    void *walkable;
+    void *player[4];
+    void *grinch;
+}                   t_img;
+
+typedef struct s_data
+{
+    void *mlx;
+    void *window_ptr;
+    t_img	img;
+    t_coord coord;
+}                   t_data;
+
+#endif
