e9251197cc2c9127368dba0dfb4fe996c9eef2df
papavero42 <141956427+papavero42@users.noreply.github.com>
2023-12-19 19:53:53 +0100

    Update ft_render.c

--- ft_render.c
+++ ft_render.c
@@ -1,19 +1,22 @@
+#include "so_long.h"
 void	ft_render(t_data *window)
 {
-    window->img.exit= mlx_xpm_file_to_image(window->mlx, "", //casetta
-			&window->size.x, &window->size.y);
-    window->img.wall = mlx_xpm_file_to_image(window->mlx, "", //albero
-			&window->size.x, &window->size.y);
+    window->img.exit= mlx_xpm_file_to_image(window->mlx, "house.xpm", //casetta
+			&window->coord.x, &window->coord.y);
+    window->img.wall = mlx_xpm_file_to_image(window->mlx, "christmas_tree.xpm", //albero
+			&window->coord.x, &window->coord.y);
     window->img.coll = mlx_xpm_file_to_image(window->mlx, //regalo
-			"", &window->size.x, &window->size.y);
+			"present1.xpm", &window->coord.x, &window->coord.y);
     window->img.walkable= mlx_xpm_file_to_image(window->mlx, //neve
-			"", &window->size.x, &window->size.y);
+			"snow.xpm", &window->coord.x, &window->coord.y);
 	window->img.player[0] = mlx_xpm_file_to_image(window->mlx, //babbo natale verso destra
-			"", &window->size.x, &window->size.y);
+			"santa_right.xpm", &window->coord.x, &window->coord.y);
 	window->img.player[1] = mlx_xpm_file_to_image(window->mlx, //babbo natale verso sinistra
-			"", &window->size.x, &window->size.y);
+			"santa_left.xpm", &window->coord.x, &window->coord.y);
 	window->img.player[2] = mlx_xpm_file_to_image(window->mlx, //babbo natale verso avanti
-			"", &window->size.x, &window->size.y);
+			"santa_front.xpm", &window->coord.x, &window->coord.y);
 	window->img.player[3] = mlx_xpm_file_to_image(window->mlx, //babbo natale verso dietro
-			"", &window->size.x, &window->size.y);
+			"santa_back.xpm", &window->coord.x, &window->coord.y);
+	window->img.grinch = mlx_xpm_file_to_image(window->mlx, //grinch
+			"grinch.xpm", &window->coord.x, &window->coord.y);
 }
