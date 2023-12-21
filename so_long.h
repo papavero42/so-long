b8347ecf56d558bd9797ea2030c610c09a2b61c3
papavero42 <141956427+papavero42@users.noreply.github.com>
2023-12-21 11:33:50 +0100

    Update so_long.h

--- so_long.h
+++ so_long.h
@@ -1,6 +1,7 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+
 # include "mlx/mlx.h"
 # include "ft_printf/ft_printf.h"
 # include "libft/libft.h"
@@ -10,6 +11,10 @@
 # define WINDOW_HEIGHT 600
 # define IMG_WIDTH 32
 # define IMG_HEIGHT 32
+# define UP 65362
+# define DOWN 65364
+# define LEFT 65361
+# define RIGHT 65363
 # define ESC 65307
 
 
@@ -36,6 +41,8 @@ typedef struct s_data
     t_img	img;
     char    **map;
     int     present;
+    t_coord santa_position;
 }                   t_data;
 
 #endif
+
